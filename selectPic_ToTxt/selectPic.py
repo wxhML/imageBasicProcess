edcd757d68ecd1ee3a10e41d459b36c50b0bb572
wxhML <1096245155@qq.com>
2020-11-03 18:14:25 +0800

    git commit "selectPic"

---
+++ selectPic_ToTxt/selectPic.py
@@ -0,0 +1,23 @@
+import shutil
+def select(filepath,des):
+    selectList = []
+    with open(filepath, 'r', encoding='gbk') as readFile:  #按照gbk的编码来读取文件
+        flag = False
+        for line in readFile.readlines():
+            line = line.replace("\n","")
+            if line.startswith("lizi:"):
+                if line.find("AF") == -1:
+                    flag = True
+                    shutil.move(line.split("___")[-1][5:], "F:/pic/大兴/test/框错误")
+
+
+            # if flag == True:
+            #     if line.startswith("name:"):
+            #         selectList.append(line[5:])
+            #         shutil.move(readFile[5:], "F:/pic/大兴/test/框错误")  #直接move，no copy
+            #         flag = False
+    print(len(selectList))
+
+
+if __name__ == "__main__":
+    select("../result_daxin_modify.txt","")
