0922262c30a1a67aa21f0925671b3fbc8c4717ff
wxhML <1096245155@qq.com>
2020-11-05 10:17:00 +0800

    git commit "calcualteAccuracy"

--- CalculateAcuuracy/accuracy.py
+++ CalculateAcuuracy/accuracy.py
@@ -1,4 +1,5 @@
 import shutil
+import os
 def accuracy(filePath):
     right = 0
     error = 0
@@ -14,19 +15,34 @@ def accuracy(filePath):
             if flag:
                 predictLabel = line
                 if truthLabel == predictLabel:
+                # if predictLabel.find("民航")  != -1:
                     right += 1
-                    shutil.move(picName, "F:/pic/大兴机场最终素材/test/right")
+                    if not os.path.exists('F:/pic/大兴机场最终素材/test/right'):
+                        os.mkdir("F:/pic/大兴机场最终素材/test/right")
+                    # shutil.move(picName, "F:/pic/大兴机场最终素材/test/right")
                 else:
                     error += 1
-                    shutil.move(picName, "F:/pic/大兴机场最终素材/test/right")
+                    if not os.path.exists('F:/pic/大兴机场最终素材/test/error'):
+                        os.mkdir("F:/pic/大兴机场最终素材/test/error")
+                    # shutil.move(picName, "F:/pic/大兴机场最终素材/test/error")
                 print(truthLabel)
                 print(predictLabel)
                 flag = False
             if line.find("name:") != -1:
+                lizi = line[:line.find("name:")]
                 truthLabel = line.split("_")[-1][:-4]
-                picName = line[line.find("name:")+5:]
+                picName = line[line.find("name:") + 5:]
+                if lizi.find("AF") == -1:
+                    if not os.path.exists('F:/pic/大兴机场最终素材/test/NOAF1'):
+                            os.mkdir("F:/pic/大兴机场最终素材/test/NOAF1")
+                    # shutil.move(picName, "F:/pic/大兴机场最终素材/test/NOAF1")
 
-
+                # if lizi.find("A") == -1 or lizi.find("F") == -1 and :
+                # if lizi.find("U") != -1:
+                # if lizi.find("A") != -1:
+                #     if not os.path.exists('F:/pic/大兴机场最终素材/test/SHIBIE1'):
+                #         os.mkdir("F:/pic/大兴机场最终素材/test/SHIBIE1")
+                    # shutil.move(picName, "F:/pic/大兴机场最终素材/test/SHIBIE1")
                 flag = True
                 # print(truthLabel)
                 index += 1
@@ -40,4 +56,4 @@ def accuracy(filePath):
 
 
 if __name__ == "__main__":
-    accuracy("../result_daxin_modify0.txt")
+    accuracy("../result_daxin_modify_modifyRight_5error.txt")
